8248490414a7052b022971ed2fd558ad1f1b6a87
John <jkespinosa.25@yahoo.com>
2025-10-26 23:54:56 -0400

    answered until 33

--- Automaton.java
+++ Automaton.java
@@ -51,24 +51,31 @@ public class Automaton
         // Naively update the state of each cell
         // based on the state of its two neighbors.
         for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            if(i == 0) {
-                left = 0;
-            }
-            else {
-                left = state[i - 1];
-            }
-            center = state[i];
-            if(i + 1 < state.length) {
-                right = state[i + 1];
-            }
-            else {
-                right = 0;
-            }
+            int left = (i == 0) ? 0 : state[i - 1];
+            int center = state[i];
+            int right = (i == state.length - 1) ? 0 : state[i + 1];
+            
             nextState[i] = (left + center + right) % 2;
         }
         state = nextState;
     }
+    
+    public int calculateNextState(int left, int center, int right){
+        return (left + center + right) % 2;
+    }
+    
+    public void updateIterator(){
+        int[] nextState = new int[state.length];
+        int left = 0;
+        int center = state[0];
+        for (int i = 0; i < state.length; i++) {
+             int right = (i < state.length - 1) ? state[i + 1] : 0;
+             nextState[i] = calculateNextState(left, center, right);
+             left = center;
+             center = right;
+        }
+        state = nextState;
+    } 
     
     /**
      * Reset the automaton.
@@ -78,5 +85,7 @@ public class Automaton
         Arrays.fill(state, 0);
         // Seed the automaton with a single 'on' cell.
         state[numberOfCells / 2] = 1;
+        state[numberOfCells / 2 - 1] = 1;
+        state[numberOfCells / 2 + 1] = 1;
     }
 }
